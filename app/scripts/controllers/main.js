529d199d1f296a360b78d724778b21ea81fa8c04
bzhang <Bin.Zhang@blackboard.com>
2015-01-28 20:44:29 +0800

    init project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name betGameAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the betGameAngularApp
+ */
+angular.module('betGameAngularApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
